4cf256154f13f0f0526b1bbcd3cff08eb4ed62f9
AngelPlayer <ssh5212@naver.com>
2023-12-12 16:46:38 +0900

    init: back setting

---
+++ BE/ClassManager/src/test/java/kr/co/angelplayer/class_manager/ClassManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package kr.co.angelplayer.class_manager;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
